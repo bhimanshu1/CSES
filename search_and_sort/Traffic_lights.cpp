686a593546a83d05ce1eaf5a1a32628d8e7a7afb
bhimanshu1 <bhimanshu7274@gmail.com>
2024-11-22 11:10:35 +0530

    new problem

---
+++ search_and_sort/Traffic_lights.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int MOD = 1e9 + 7;
+#define pi 3.14159265359
+const ll lmax = 1e18;
+const int imax = 1e9;
+#define all(a) a.begin(), a.end()
+
+int solve()
+{
+    int x, n;
+    cin >> x >> n;
+
+    multiset<int> ms;
+    ms.insert(0);
+    ms.insert(x);
+
+    ll pos;
+    multiset<int> ms2;
+    ms2.insert(x);
+    for (int i = 0; i < n; i++){
+        cin >> pos;
+
+        auto it = ms.upper_bound(pos);   // closest bigger value
+        auto sm = prev(it);   // closest smallest value;
+
+        int lft = *sm;
+        int rgt  = *it;
+        ms2.erase(ms2.find(rgt - lft));  // now delete the max diff
+
+        ms2.insert((pos - lft));  // add the smaller differences
+        ms2.insert((rgt - pos));  // add the larget differences
+        ms.insert(pos);
+
+        cout << *(prev(ms2.end())) << " ";
+    }
+
+    return 0;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);    // to read the input from the input.txt
+    // freopen("output.txt", "w", stdout);  // to read the output to the output.txt
+    ios_base::sync_with_stdio(0);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    // int t;
+    // cin >> t;
+    // while (t--)
+    // {
+
+    solve();
+    // }
+
+    return 0;
+}
